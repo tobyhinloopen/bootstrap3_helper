23b0f10efe3997557c3c01d791b84ba3dfe50136
Toby Hinloopen <toby@zisoo.nl>
2016-08-08 12:43:15 +0200

    Added grid helpers

--- lib/bootstrap3_helper/grid.ex
+++ lib/bootstrap3_helper/grid.ex
@@ -5,4 +5,18 @@ defmodule Bootstrap3Helper.Grid do
   def container(content) do
     content_tag :div, content, class: "container"
   end
+
+  def row([do: content]), do: row(content)
+  def row(content) do
+    content_tag :div, content, class: "row"
+  end
+
+  def col(opts, [do: content]), do: col(content, opts)
+  def col(content, opts) do
+    classnames = [
+      ([:xs, :sm, :md, :lg] |> Enum.map(fn k -> Keyword.has_key?(opts, k) && " col-#{k}-#{opts[k]}" || "" end)) |
+      ([:xs_offset, :sm_offset, :md_offset, :lg_offset] |> Enum.map(fn k -> Keyword.has_key?(opts, k) && " col-#{k}-offset-#{opts[k]}" || "" end))
+    ]
+    content_tag :div, content, class: "col" <> Enum.join(classnames, "")
+  end
 end
--- test/bootstrap3_helper/grid_test.exs
+++ test/bootstrap3_helper/grid_test.exs
@@ -17,4 +17,24 @@ defmodule Bootstrap3Helper.Components.GridTest do
     assert safe_to_string(Grid.container "Foo")
       == "<div class=\"container\">Foo</div>"
   end
+
+  test "row with content param creates DIV.row" do
+    assert safe_to_string(Grid.row "Foo")
+      == "<div class=\"row\">Foo</div>"
+  end
+
+  test "row with content block creates DIV.row" do
+    assert safe_to_string(Grid.row do: "Foo")
+      == "<div class=\"row\">Foo</div>"
+  end
+
+  test "col with content block creates DIV.col" do
+    assert safe_to_string(Grid.col [sm: 4], do: "Foo")
+      == "<div class=\"col col-sm-4\">Foo</div>"
+  end
+
+  test "col with content param creates DIV.col" do
+    assert safe_to_string(Grid.col "Foo", sm: 4)
+      == "<div class=\"col col-sm-4\">Foo</div>"
+  end
 end
